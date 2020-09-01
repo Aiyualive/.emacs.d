3a8607243348fa66467c0e6b70a82ae13ddd8f9d
Aiyu Liu <mqt615@alumni.ku.dk>
2020-09-01 11:26:22 +0200

    modified .gitignore

--- elpa/s-20180406.808/s-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; s-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("s.el") (23471 26148 943719 857000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; s-autoloads.el ends here
